294892cee70ef0f0b7eaf1505d90312e894d78e5
GeoClawAgent <geoclaw.agen@gmail.com>
2026-04-16 11:24:37 -0600

    chore: remove CTA v2 from Services page

--- pages/services.php
+++ pages/services.php
@@ -8,6 +8,5 @@ get_header();
 
 <?php get_template_part('templates/breadcrumb'); ?>
 <?php get_template_part('templates/services-page'); ?>
-<?php get_template_part('templates/cta-v2'); ?>
 
 <?php get_footer(); ?>
