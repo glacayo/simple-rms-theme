20c6603f713460f917029922a5ba55552b29a1b5
Mario Delgado <mario@ravenmarketingsvcs.com>
2026-05-25 21:36:59 -0600

    refactor: remove legacy seo-content-reverse template

--- pages/landing-page.php
+++ pages/landing-page.php
@@ -7,12 +7,12 @@ get_header();
 
 get_template_part('templates/hero');
 get_template_part('templates/breadcrumb-slim');
-get_template_part('templates/seo-content-reverse');
+get_template_part('templates/seo-content');
 get_template_part('templates/vision-mission-v1');
 get_template_part('templates/badges');
 get_template_part('templates/portfolio-v1');
 get_template_part('templates/seo-content');
 get_template_part('templates/testimonials-v1');
-get_template_part('templates/seo-content-reverse');
+get_template_part('templates/seo-content');
 
 get_footer();
